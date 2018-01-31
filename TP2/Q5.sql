38ca3965059b6f60a925a52da142694beaf9d735
BenoitLB <benoit.lebad@hotmail.com>
2018-01-31 19:07:31 +0100

    TP2 commenté terminé

--- TP2/Q5.sql
+++ TP2/Q5.sql
@@ -1,4 +1,7 @@
+--Ajout de l'attribut prix, de type numérique à la relation Achats
 ALTER TABLE Achats ADD prix NUMBER(*,2);
 
+
+--Requêtes permettant de remplir le prix des Achats
 UPDATE Achats SET prix = 10.80 WHERE refl = 'ISBN46545458';
 UPDATE Achats SET prix = 30.70 WHERE refl = 'ISBN12111221';
--- TP2/Q6.sql
+++ TP2/Q6.sql
@@ -1,11 +1,15 @@
 SET ECHO OFF
 SPOOL 2013-01-28-achats.lst
-SET ECHO OFF
 SET FEEDBACK OFF
 SET HEADING ON
 SET LINESIZE 50
 SET PAGESIZE 60
+
+-- Titre du rapport
 TTITLE CENTER 'Achats des clients au 28 janvier 2013'
+
+-- Suppression de la ligne de '_' au debut du tableau
+SET UNDERLINE OFF
 
 -- Création de colonnes
 COLUMN idcl   HEADING 'Idcl'
@@ -13,10 +17,19 @@ COLUMN dateachat  HEADING 'Date d''achat'  FORMAT A14
 COLUMN genre   HEADING 'Genre'
 COLUMN prix   HEADING 'Prix'  FORMAT 999.90
 
+-- Affiche les moyennes et les somme par client
+BREAK ON idcl SKIP 1
+COMPUTE AVG LABEL 'avg (moy)' -
+SUM LABEL 'sum (som)' OF prix ON idcl
+
+-- Requete permettant d'afficher ce qu'on souhaite afficher
 SELECT idcl, dateachat, genre, prix
 FROM Achats NATURAL JOIN Livres
-WHERE dateachat < '28-JAN-2013';
+WHERE dateachat < '28-JAN-2013'
+GROUP BY (idcl, dateachat, genre, prix)
+ORDER BY (idcl);
 
 
 SPOOL OFF
-SET ECHO ON
+SET ECHO OFF
+SET FEEDBACK ON
