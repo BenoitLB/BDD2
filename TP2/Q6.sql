bbeaaac523a097a19d38bd108945055d3a555c39
BenoitLB <benoit.lebad@hotmail.com>
2018-01-31 13:59:23 +0100

    Presque fini faut faire la bonne commande

--- TP2/Q6.sql
+++ TP2/Q6.sql
@@ -2,11 +2,21 @@ SET ECHO OFF
 SPOOL 2013-01-28-achats.lst
 SET ECHO OFF
 SET FEEDBACK OFF
-SET HEADING OFF
-SET PAGESIZE 0
-"idcl   date achats   genre   prix  "
+SET HEADING ON
+SET LINESIZE 50
+SET PAGESIZE 60
+TTITLE CENTER 'Achats des clients au 28 janvier 2013'
+
+-- Création de colonnes
+COLUMN idcl   HEADING 'Idcl'
+COLUMN dateachat  HEADING 'Date d''achat'  FORMAT A14
+COLUMN genre   HEADING 'Genre'
+COLUMN prix   HEADING 'Prix'  FORMAT 999.90
+
 SELECT idcl, dateachat, genre, prix
-FROM Achats NATURAL JOIN Livres;
+FROM Achats NATURAL JOIN Livres
+WHERE dateachat < '28-JAN-2013';
+
 
 SPOOL OFF
 SET ECHO ON
