9766c5097574a1ca10b46139aed508def3b161c2
BenoitLB <benoit.lebad@hotmail.com>
2018-02-21 19:22:06 +0100

    Deuxième partie terminée

--- TP5/Part2.sql
+++ TP5/Part2.sql
@@ -119,3 +119,68 @@ select * from table(dbms_xplan.display);
 --  PLAN_TABLE_OUTPUT
 --  ------------------------------------------------------------------------------------------------------------------------------------------------------
 --     2 - access("NB_PAGES">20)
+
+
+
+
+EXPLAIN PLAN FOR SELECT title FROM publications1 NATURAL JOIN facts WHERE date_id = 2008;
+select * from table(dbms_xplan.display);
+--  ------------------------------------------------------------------------------------
+--  | Id  | Operation	         | Name	         | Rows  | Bytes | Cost (%CPU)| Time	   |
+--  ------------------------------------------------------------------------------------
+--  |   0 | SELECT STATEMENT   |		           | 37407 |  4895K|  3623   (1)| 00:00:44 |
+--  |*  1 |  HASH JOIN	       |		           | 37407 |  4895K|  3623   (1)| 00:00:44 |
+--  |*  2 |   TABLE ACCESS FULL| FACTS	       | 37258 |  1237K|  1677   (1)| 00:00:21 |
+--  |   3 |   TABLE ACCESS FULL| PUBLICATIONS1 |   481K|	  45M|  1944   (1)| 00:00:24 |
+--  ------------------------------------------------------------------------------------
+--  PLAN_TABLE_OUTPUT
+--  --------------------------------------------------------------------------------------------------------------------------------------------
+--  Predicate Information (identified by operation id):
+--  ---------------------------------------------------
+--     1 - access("PUBLICATIONS1"."PUBLICATION_ID"="FACTS"."PUBLICATION_ID")
+--     2 - filter(TO_NUMBER("FACTS"."DATE_ID")=2008)
+
+
+
+
+EXPLAIN PLAN FOR SELECT title FROM publications1 WHERE publication_id IN (SELECT publication_id FROM facts WHERE date_id = 2008);
+select * from table(dbms_xplan.display);
+--  --------------------------------------------------------------------------------------
+--  | Id  | Operation	           | Name	         | Rows  | Bytes | Cost (%CPU)| Time     |
+--  --------------------------------------------------------------------------------------
+--  |   0 | SELECT STATEMENT     |		           | 37401 |	4894K|	3623   (1)| 00:00:44 |
+--  |*  1 |  HASH JOIN RIGHT SEMI|		           | 37401 |	4894K|	3623   (1)| 00:00:44 |
+--  |*  2 |   TABLE ACCESS FULL  | FACTS	       | 37258 |	1237K|	1677   (1)| 00:00:21 |
+--  |   3 |   TABLE ACCESS FULL  | PUBLICATIONS1 |	 481K|	  45M|	1944   (1)| 00:00:24 |
+--  --------------------------------------------------------------------------------------
+--  PLAN_TABLE_OUTPUT
+--  --------------------------------------------------------------------------------------------------------------------------------------------
+--  Predicate Information (identified by operation id):
+--  ---------------------------------------------------
+--     1 - access("PUBLICATION_ID"="PUBLICATION_ID")
+--     2 - filter(TO_NUMBER("DATE_ID")=2008)
+
+
+
+EXPLAIN PLAN FOR SELECT title FROM publications1 NATURAL JOIN facts NATURAL JOIN collaborations NATURAL JOIN authors WHERE last_name = 'Rosenthal';
+select * from table(dbms_xplan.display);
+--  ---------------------------------------------------------------------------------------
+--  | Id  | Operation	           | Name	          | Rows  | Bytes | Cost (%CPU)| Time     |
+--  ---------------------------------------------------------------------------------------
+--  |   0 | SELECT STATEMENT     |		            |     5 |  1120 |    6474	(1)| 00:01:18 |
+--  |*  1 |  HASH JOIN	         |		            |     5 |  1120 |    6474	(1)| 00:01:18 |
+--  |*  2 |   HASH JOIN	         |		            |     5 |   620 |   4527	(1)| 00:00:55 |
+--  |*  3 |    HASH JOIN	       |		            |     5 |   340 |    2850	(1)| 00:00:35 |
+--  |*  4 |     TABLE ACCESS FULL| AUTHORS	      |     2 |    50 |    618	(1)| 00:00:08 |
+--  |   5 |     TABLE ACCESS FULL| COLLABORATIONS |  1212K|    49M|   2227	(1)| 00:00:27 |
+--  PLAN_TABLE_OUTPUT
+--  ---------------------------------------------------------------------------------------
+--  |   6 |    TABLE ACCESS FULL | FACTS	        |   473K|    25M|    1676	(1)| 00:00:21 |
+--  |   7 |   TABLE ACCESS FULL  | PUBLICATIONS1  |   481K|    45M|   1944	(1)| 00:00:24 |
+--  ---------------------------------------------------------------------------------------
+--  Predicate Information (identified by operation id):
+--  ---------------------------------------------------
+--     1 - access("PUBLICATIONS1"."PUBLICATION_ID"="FACTS"."PUBLICATION_ID")
+--     2 - access("FACTS"."SQUAD_ID"="COLLABORATIONS"."SQUAD_ID")
+--     3 - access("COLLABORATIONS"."AUTHOR_ID"="AUTHORS"."AUTHOR_ID")
+--     4 - filter("AUTHORS"."LAST_NAME"='Rosenthal')
